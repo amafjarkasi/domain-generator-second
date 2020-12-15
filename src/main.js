2588423db97cf3323718f9972da43318e6b95941
amafjarkasi <amafjarkasi@gmail.com>
2020-12-15 01:16:34 +0000

    latest updates

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "best"];
+let adj = ["great", "big", "amazing", "funny", "sleepy"];
+let noun = ["jogger", "racoon", "mother"];
+let tld = [".com", ".net", ".org", ".us", ".biz", ".co"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+for (
+  var pronoun_counter = 0;
+  pronoun_counter < pronoun.length;
+  pronoun_counter++
+) {
+  for (var adj_counter = 0; adj_counter < adj.length; adj_counter++) {
+    for (var noun_counter = 0; noun_counter < noun.length; noun_counter++) {
+      for (var tld_counter = 0; tld_counter < tld.length; tld_counter++) {
+        console.log(
+          pronoun[pronoun_counter] +
+            adj[adj_counter] +
+            noun[noun_counter] +
+            tld[tld_counter]
+        );
+      }
+    }
+  }
+}
